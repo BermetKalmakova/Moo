e5d1a7208cadddbce671994bd8ae09672836911d
BermetKalmakova <bermetkalmakova@gmail.com>
2017-12-12 18:36:45 -0500

    done

--- find.js
+++ find.js
@@ -5,8 +5,8 @@ var style = document.getElementsByTagName("style")[0];
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
+var targetX = Math.random() * boxWidth;
+var targetY = Math.random() * boxHeight;
 
 
 console.log( "box height: " + boxHeight );
@@ -17,20 +17,18 @@ var distance = function (x0, y0, x1, y1) {
     return (((x0 - x1) ** 2) + ((y0-y1) ** 2)) ** .5;
 };
 
+
 var transparency = 1;
+var maxd = (((boxHeight) ** 2) + ((boxWidth) ** 2)) ** .5;
 
 var findIt = function(e) {
-    
-  /* YOUR IMPLEMENTATION */
+    transparency = distance(e.pageX, e.pageY, targetX, targetY)/maxd;
+    setColor();
 };
-
-/*
-your OTHER FXNS
-*/
 
 
 var setColor = function(e) {
-    style.innerHTML = style.innerHTML + ".box {background-color: #000000; opacity: " + transparency + ";}";
-}
+    style.innerHTML = ".full {height: 100vh;}.box {border: 1px solid black;background-color: #311B55; opacity: " + transparency + ";}";
+};
 
 box.addEventListener("mousemove", findIt);
